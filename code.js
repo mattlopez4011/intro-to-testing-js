27d304b91a653fe7f52c039dfa54f40ee756c460
Matt Lopez <matthew.lopez4011@gmail.com>
2019-08-12 10:41:42 -0500

    Complete TDD exercise 6

--- code.js
+++ code.js
@@ -10,13 +10,14 @@ var helloWorld = function(){
 };
 
 function sayHello(input) {
-    if (input === "Alex"){
-        return "Hello, Alex!"
-    }else if(input === "Jane"){
-        return "Hello, Jane!"
-    }else{
-        return "Hello, Pat!"
-    }
+    return "Hello, " + input + "!";
+    // if (input === "Alex"){
+    //     return "Hello, Alex!"
+    // }else if(input === "Jane"){
+    //     return "Hello, Jane!"
+    // }else{
+    //     return "Hello, Pat!"
+    // }
     // return "Hello, Jane!";
 }
 
